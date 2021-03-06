f2c37ff09e48df5029ad46b4f7549d01cce668a9
liam-montero <78785980+liam-montero@users.noreply.github.com>
2021-03-06 11:56:07 +0100

    Génération trait vert tous les 20

--- space_invaders/space_invaders.srcs/sources_1/new/generate_test_screen.vhd
+++ space_invaders/space_invaders.srcs/sources_1/new/generate_test_screen.vhd
@@ -2,7 +2,7 @@
 -- Company: 
 -- Engineer: 
 -- 
--- Create Date: 02.03.2021 16:03:38
+-- Create Date: 06.03.2021 10:45:18
 -- Design Name: 
 -- Module Name: generate_test_screen - Behavioral
 -- Project Name: 
@@ -21,6 +21,8 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+library xil_defaultlib;
+use xil_defaultlib.space_invaders_package.all;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -30,61 +32,38 @@ use IEEE.NUMERIC_STD.ALL;
 -- any Xilinx leaf cells in this code.
 --library UNISIM;
 --use UNISIM.VComponents.all;
-library xil_defaultlib;
-use xil_defaultlib.space_invaders_package.all;
 
 entity generate_test_screen is
-    Port ( 
-        rst_in  : in std_logic;
-        clk_in  : in std_logic;
-        en_clear_in : in std_logic;
-        end_gen_out   : out std_logic;
-        memory_out  : out screen_memory
-    );
+  Port ( clk     : in std_logic;
+         rst     : in std_logic;
+         en_gen  : in std_logic;
+         addr    : in std_logic_vector(13 downto 0);
+         data_out: out screen_memory
+        );
 end generate_test_screen;
 
 architecture Behavioral of generate_test_screen is
-
-    signal en_cnt   : std_logic := '0';
-    signal addr     : std_logic_vector(13 downto 0); -- A changer si format différent de 160*100
-
+signal sig_data_out : screen_memory;
 begin
-
-    inst_cpt : entity work.compteur_generique
-    generic map(NbBit => 14)
-    port map (
-        rst_in => rst_in,      
-        clk_in => clk_in,  
-        clk_enable_in => '1',  
-        init_in => '0',     
-        load_in => '0',     
-        enable_in => en_cnt,   
-        data_in => (others => '0'),     
-        data_out => addr 
-    );
-
-    process(rst_in, clk_in)
-    begin
-        if(rst_in = '0') then 
-
-            en_cnt  <= '0';
-            end_gen_out   <= '0';
-            memmor
-
-        elsif (clk_in'event and clk_in = '1') then
-
-            if(en_clear_in = '1') then
-                en_cnt  <= '1';
-            elsif(addr >= std_logic_vector(to_unsigned(15999, 14))) then
-                memory_out(to_integer(unsigned(addr)))  <= "000";
-                en_cnt  <= '0';
-                end_gen_out   <= '1';
-            elsif(to_integer(unsigned(addr)) mod 20 = 0) then
-                memory_out(to_integer(unsigned(addr)))  <= "010";
-            end if;
     
-        end if; 
-
+    process(clk, rst)
+    begin
+    
+     if (rst = '1') then
+        for i in 0 to 15999 loop
+            sig_data_out(i) <= "111"; -- Blanc
+        end loop;
+     elsif (clk'event and clk = '1') then 
+        if en_gen = '1' then
+            if( to_integer(unsigned(addr)) mod 20 = 0 ) then
+                sig_data_out(to_integer(unsigned(addr))) <= "010";-- Vert
+            else
+                sig_data_out(to_integer(unsigned(addr))) <= "111";-- Blanc
+            end if; 
+        end if;
+     end if;
     end process;
-
+   
+    data_out <= sig_data_out;
+    
 end Behavioral;
